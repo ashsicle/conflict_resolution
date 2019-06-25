5186bdfccc26b7799b522d356507f947704d9154
ashsicle <48106072+ashsicle@users.noreply.github.com>
2019-06-25 15:31:12 -0400

    Update map.js

--- map.js
+++ map.js
@@ -7,7 +7,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYXNoc2ljbGUiLCJhIjoiY2pzcGlqeDFrMHh3dTQ1bGp5c
 let map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/ashsicle/cjx27mx0p697f1cp92ct3p2id',
-    center: [-73.96216,40.80779],
+    center: [-66.473054,18.220031],
     zoom: 16
 })
 

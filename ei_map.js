9e77157283246b7a916296348efdaf47dda6a771
ashsicle <48106072+ashsicle@users.noreply.github.com>
2019-06-25 14:55:41 -0400

    Create ei_map.js

---
+++ ei_map.js
@@ -0,0 +1,38 @@
+'use strict'
+
+console.log('Loaded map.js')
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXNoc2ljbGUiLCJhIjoiY2pzcGlqeDFrMHh3dTQ1bGp5c3ozN3N4ciJ9.Bg-amNI9jqeWLDkQMylkmQ'
+
+let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/ashsicle/cjx27mx0p697f1cp92ct3p2id',
+    center: [-73.96216,40.80779],
+    zoom: 16
+})
+
+let navigation = new mapboxgl.NavigationControl({
+    showCompass: false
+})
+map.addControl(navigation, 'top-left')
+
+let scale = new mapboxgl.ScaleControl({
+    maxWidth: 80,
+    unit: 'imperial'
+})
+map.addControl(scale, 'bottom-right')
+
+let geolocate = new mapboxgl.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    trackUserLocation: true,
+    showUserLocation: true,
+    fitBoundsOptions: {
+    }
+})
+map.addControl(geolocate, 'top-left')
+
+geolocate.on('geolocate', function(event) {
+
+})

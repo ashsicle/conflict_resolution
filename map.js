8fa8683ed294fc72b1e439070f159db334cd76ac
ashsicle <48106072+ashsicle@users.noreply.github.com>
2019-06-25 15:43:08 -0400

    Update map.js

--- map.js
+++ map.js
@@ -8,7 +8,7 @@ let map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/ashsicle/cjx27mx0p697f1cp92ct3p2id',
     center: [-66.473054,18.220031],
-    zoom: 10
+    zoom: 9
 })
 
 let navigation = new mapboxgl.NavigationControl({
@@ -21,21 +21,6 @@ let scale = new mapboxgl.ScaleControl({
     unit: 'imperial'
 })
 map.addControl(scale, 'bottom-right')
-
-let geolocate = new mapboxgl.GeolocateControl({
-    positionOptions: {
-        enableHighAccuracy: true
-    },
-    trackUserLocation: true,
-    showUserLocation: true,
-    fitBoundsOptions: {
-    }
-})
-map.addControl(geolocate, 'top-left')
-
-geolocate.on('geolocate', function(event) {
-
-})
 
 
 // mapboxgl.accessToken = 'pk.eyJ1IjoiYXNoc2ljbGUiLCJhIjoiY2pzcGlqeDFrMHh3dTQ1bGp5c3ozN3N4ciJ9.Bg-amNI9jqeWLDkQMylkmQ';

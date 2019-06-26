b66a32fb5625e1ab0c4ece297f2e673cf3400cbd
ashsicle <48106072+ashsicle@users.noreply.github.com>
2019-06-26 18:19:41 -0400

    Create map_ei.js

---
+++ map_ei.js
@@ -0,0 +1,12 @@
+'use strict'
+
+console.log('Loaded map_ei.js')
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXNoc2ljbGUiLCJhIjoiY2pzcGlqeDFrMHh3dTQ1bGp5c3ozN3N4ciJ9.Bg-amNI9jqeWLDkQMylkmQ'
+
+// let map = new mapboxgl.Map({
+//     container: 'map',
+//     style: 'mapbox://styles/ashsicle/cjx27mx0p697f1cp92ct3p2id',
+//     center: [-66.473054,18.220031],
+//     zoom: 8
+// })

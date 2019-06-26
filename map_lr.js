495fec2e55e28917975ba20a1bd392d2ee46551b
ashsicle <48106072+ashsicle@users.noreply.github.com>
2019-06-26 19:20:23 -0400

    Create map_lr.js

---
+++ map_lr.js
@@ -0,0 +1,14 @@
+'use strict'
+
+console.log('Loaded map_ei.js')
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXNoc2ljbGUiLCJhIjoiY2pzcGlqeDFrMHh3dTQ1bGp5c3ozN3N4ciJ9.Bg-amNI9jqeWLDkQMylkmQ'
+
+let map = new mapboxgl.Map ({
+  container: 'map',
+  style: 'mapbox://styles/ashsicle/cjxceevy1056w1hphlovb8qd1',
+  center: [-66.473054,18.220031],
+  zoom: 8
+})
+        
+var toggleableLayerIds = ['Local Residents Decision Map', 'Population Density', 'Community Services', 'Health Services', 'Education Services'];

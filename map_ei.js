a19a70fc3fad4b90a176d42cd2cf13cbf9df6f1d
ashsicle <48106072+ashsicle@users.noreply.github.com>
2019-06-26 18:21:03 -0400

    Update map_ei.js

--- map_ei.js
+++ map_ei.js
@@ -4,9 +4,11 @@ console.log('Loaded map_ei.js')
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYXNoc2ljbGUiLCJhIjoiY2pzcGlqeDFrMHh3dTQ1bGp5c3ozN3N4ciJ9.Bg-amNI9jqeWLDkQMylkmQ'
 
-// let map = new mapboxgl.Map({
-//     container: 'map',
-//     style: 'mapbox://styles/ashsicle/cjx27mx0p697f1cp92ct3p2id',
-//     center: [-66.473054,18.220031],
-//     zoom: 8
-// })
+let map = new mapboxgl.Map ({
+  container: 'map',
+  style: 'mapbox://styles/ashsicle/cjx27mx0p697f1cp92ct3p2id',
+  center: [-66.473054,18.220031],
+  zoom: 8
+})
+        
+var toggleableLayerIds = ['External Investors Decision Map', 'Hotels', 'Outdoor Leisure', 'Urban Destinations', 'Development Areas'];

1302ffe7506f988b986567ee4c827d186aa7f230
Ashley Louie <48106072+ashsicle@users.noreply.github.com>
2020-08-01 16:11:59 -0400

    Update map_lr.js

--- map_lr.js
+++ map_lr.js
@@ -10,5 +10,18 @@ let map = new mapboxgl.Map ({
   center: [-66.473054,18.220031],
   zoom: 8
 })
+
+// map.on('load', function() {
+//   map.addLayer({
+//     id: 'raster-layer',
+//     type: 'raster',
+//     source: {
+//       type: 'raster',
+//       tiles: ['https://api.mapbox.com/v4/{tileset_id}/{z}/{x}/{y}.png?access_token=YOUR_MAPBOX_ACCESS_TOKEN'],
+//     },
+//     minzoom: 0,
+//     maxzoom: 22
+//   });
+// });
         
 var toggleableLayerIds = ['Local Residents Decision Model', 'Population Density', 'Community Services', 'Health Services', 'Education Services'];
